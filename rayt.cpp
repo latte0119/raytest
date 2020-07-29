0c29323059b6b1d74a1f15a518d921b2f9bebfd3
latte0119 <latte.malta@gmail.com>
2020-07-29 21:44:01 +0900

    ray&camera

--- rayt.cpp
+++ rayt.cpp
@@ -1,24 +1,39 @@
 #include "rayt.hpp"
 #include<memory>
-
+#include<iostream>
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image.h"
 #include "stb/stb_image_write.h"
 
 
+vec3 color(const rayt::Ray& r){
+    vec3 d=normalize(r.direction());
+    double t=(d.getY()+1.0)/2;
+    return lerp(t,vec3(0.3,0.6,0.9),vec3(0,0,0));
+}
+
 int main(){
     int H=100;
     int W=200;
     std::unique_ptr<rayt::Image>image(std::make_unique<rayt::Image>(H,W));
 
+    vec3 x(4,0,0);
+    vec3 y(0,2,0);
+    vec3 z(-2,-1,-1);
+
+    std::unique_ptr<rayt::Camera>camera(std::make_unique<rayt::Camera>(x,y,z));
+
     for(int j=0;j<H;j++){
         for(int i=0;i<W;i++){
-            double r=1.0*i/W;
-            double g=1.0*j/H;
-            double b=0.5;
+            double u=1.0*i/W;
+            double v=1.0*j/H;
 
-            image->write(i,j,r,g,b);
+            rayt::Ray ray=camera->getRay(u,v);
+            
+            vec3 c=color(ray);
+
+            image->write(i,j,c.getX(),c.getY(),c.getZ());
         }
     }
 
--- rayt.hpp
+++ rayt.hpp
@@ -1,5 +1,24 @@
 #include<memory>
+#include<random>
+#include "sce_vectormath/include/vectormath/scalar/cpp/vectormath_aos.h"
+
+
+using namespace Vectormath::Aos;
+using vec3=Vector3;
+using col3=Vector3;
+std::random_device seed_gen;
+std::default_random_engine engine(seed_gen());
+std::uniform_real_distribution<double>urd(0.0,1.0);
+inline double drand48(){
+    return urd(engine);
+}
+const double PI=acos(-1);
+inline double radians(const double deg){return deg/180*PI;}
+inline double degrees(const double rad){return rad/PI*180;}
+
+
 namespace rayt{
+    
     class Image{
         public:
         struct rgb{
@@ -27,5 +46,53 @@ namespace rayt{
         int m_height;
         int m_width;
         std::unique_ptr<rgb[]>m_pixels;
+    };
+
+    class Ray{
+        public:
+        Ray(){}
+        Ray(const vec3& o,const vec3& dir):m_origin(o),m_direction(dir){}    
+
+        const vec3& origin()const{return m_origin;}
+        const vec3& direction()const{return m_direction;}
+        const vec3 at(double t)const{return m_origin+t*m_direction;}
+
+        private:
+        vec3 m_origin;
+        vec3 m_direction;
+    };
+
+
+    class Camera{
+        public:
+        Camera(const vec3& u,const vec3& v,const vec3& w){
+            m_origin=vec3(0);
+            m_uvw[0]=u;
+            m_uvw[1]=v;
+            m_uvw[2]=w;
+        }
+
+        Camera(const vec3& lookfrom,const vec3& lookat, const vec3& vup,double vfov,double aspect){
+            const vec3 Z=normalize(lookat-lookfrom);
+            const vec3 X=normalize(cross(vup,Z));
+            const vec3 Y=cross(Z,X);
+
+            const double rad=radians(vfov);
+            const double h=tan(rad/2);
+            const double w=h*aspect;
+
+            m_uvw[0]=2*w*X;
+            m_uvw[1]=2*h*Y;
+            m_uvw[2]=lookfrom-w*X-h*Y-Z;
+            m_origin=lookfrom;
+        }
+
+        Ray getRay(double u,double v){
+            return Ray(m_origin,m_uvw[0]*u+m_uvw[1]*v+m_uvw[2]-m_origin);
+        }
+
+        private:
+        vec3 m_origin;
+        vec3 m_uvw[3];
     };
 }
